2de36d0d7670ab2ed0b258d66135647e8ea05ee6
Manuel Mendoza <mmendoza26000@gmail.com>
2020-10-12 00:05:08 -0500

    feat: Add TailwindCSS
    - with future flags enabled
    - with PurgeCSS

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#333',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
+
+// module.exports = {
+//   future: {
+//     // removeDeprecatedGapUtilities: true,
+//     // purgeLayersByDefault: true,
+//   },
+//   purge: false,
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {},
+//   plugins: [],
+// }
